09db71d9bec9c5d8864be3f1998313da89493b89
Joel Beck <joel.beck@t-online.de>
2022-05-26 18:33:41 +0200

    added test placeholder

--- tests/test.py
+++
@@ -1,5 +0,0 @@
-# NOTE: import modules from parent folders by running module from command line
-# cd into project directory scikit_learn_journey and run `python -m tests.test`
-from src.evaluation import ClassificationMetrics
-
-print("Hello")
---
+++ tests/test_default.py
@@ -0,0 +1,5 @@
+from eval_sklearn.evaluation import ClassificationMetrics
+
+
+def test_default():
+    assert 1 + 1 == 2
